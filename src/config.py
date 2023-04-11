955c8772e98587e2ebcd045a73bedf009474e043
Robert Gibboni <robert@drivendata.org>
2023-04-11 16:25:02 -0400

    Initial commit

---
+++ src/config.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+data_directory = Path(__file__).parent.parent / "data"
+
+
+AIRPORTS = [
+    "KATL",
+    "KCLT",
+    "KDEN",
+    "KDFW",
+    "KJFK",
+    "KMEM",
+    "KMIA",
+    "KORD",
+    "KPHX",
+    "KSEA",
+]
+
+AIRLINES = [
+    "AAL",
+    # "ASH",
+    "DAL",
+    "EDV",
+    # "EJA",
+    # "ENY",
+    # "FDX",
+    # "GAJ",
+    # "JIA",
+    "RPA",
+    # "SKW",
+    "SWA",
+    # "UPS",
+]
